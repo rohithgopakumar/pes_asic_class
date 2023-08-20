837b5a9c1e0bb62dc0ea4bf5732ee93f6739ef46
rohithgopakumar <131611312+rohithgopakumar@users.noreply.github.com>
2023-08-20 20:26:58 +0530

    Add files via upload

---
+++ Pictures/lab-1/unsigned.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+unsigned long long max=(unsigned long long int)(pow(2,64)*-1);
+printf("the higest number is represented unsigned long long by %llu\n",max);
+return 0;
+}
