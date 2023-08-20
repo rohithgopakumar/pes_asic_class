1873985957a65cec620d4ef4eda17aa00e25bd0c
rohithgopakumar <131611312+rohithgopakumar@users.noreply.github.com>
2023-08-20 20:28:03 +0530

    Add files via upload

---
+++ lab-1/signed.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+long long max=(long long int)(pow(2,63)-1);
+long long min=(long long int)(pow(2,63)*-1);
+printf("the higest number is represented unsigned long long by %lld\n",max);
+printf("the lowest number is represented unsigned long long by %lld\n",min);
+return 0;
+}
