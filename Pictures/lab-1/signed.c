9b046c5c94be7dfe1c9ff4fe017e728d64cbe1ad
rohithgopakumar <131611312+rohithgopakumar@users.noreply.github.com>
2023-08-20 20:27:44 +0530

    Delete Pictures directory

--- Pictures/lab-1/signed.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-int main(){
-long long max=(long long int)(pow(2,63)-1);
-long long min=(long long int)(pow(2,63)*-1);
-printf("the higest number is represented unsigned long long by %lld\n",max);
-printf("the lowest number is represented unsigned long long by %lld\n",min);
-return 0;
-}
--- Pictures/lab-1/unsigned.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-int main(){
-unsigned long long max=(unsigned long long int)(pow(2,64)*-1);
-printf("the higest number is represented unsigned long long by %llu\n",max);
-return 0;
-}
